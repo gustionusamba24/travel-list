d82c25f7efa3807ba532686cde29205bd4033447
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-06-22 16:44:21 +0700

    Building layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,40 @@
+import React from "react";
+
 const App = () => {
-  return <div>Hello React</div>;
-}
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+};
+
+const Logo = () => {
+  return <h1>Far Away</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">LIST</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>
+        You have x items on your list and you already packed X (X%)
+      </em>
+    </footer>
+  );
+};
 
 export default App;
