d120d664928ed6ee67e9e95128790982a9116f76
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-06-25 01:53:38 +0700

    Implement derive state to sort items by input, sort items by description, and sort items by packed

--- src/App.js
+++ src/App.js
@@ -84,10 +84,24 @@ const Form = ({ onAddItems }) => {
 };
 
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items.slice().sort((a, b) => a.packed - b.packed);
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -96,6 +110,17 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
